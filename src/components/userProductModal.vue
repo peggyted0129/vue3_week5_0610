cad0d2611c5006ebc70af3a0670181d7c47ac8ac
= <=>
2021-06-11 13:24:20 +0800

    0611

--- src/components/userProductModal.vue
+++ src/components/userProductModal.vue
@@ -5,26 +5,32 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header bg-sgray">
-        <h5 class="modal-title fw-bolder" id="userProductModal">{{ product.title }}</h5>
+        <h5 class="modal-title fw-bolder" id="userProductModal">{{ tempProduct.title }}</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
+      <div class="modal-body p-7">
         <div class="row">
           <div class="col-6">
-            <img :src="product.imageUrl" class="img-fluid" alt="product-pic" style="height: 230px;">
+            <img :src="tempProduct.imageUrl" class="img-fluid" alt="product-pic" style="height: 230px;">
           </div>
           <div class="col-6">
-            <p class="lh-lg mb-5">使用比利時嘉麗寶黑巧克力製作，濃郁的黑巧克力帶有苦甜的滋味，每咬一口都有滿滿的幸福感</p>
-            <div>
-              <p class="text-decoration-line-through">原價：NT$ 80</p>
-              <p class="h4 fw-bolder">售價：NT$ 60</p>
+            <span class="badge bg-secondary mt-5 mb-3">{{ tempProduct.category }}</span>
+            <p class="lh-lg mb-6">{{ tempProduct.content }}</p>
+            <div class="mb-7">
+              <p v-if="!tempProduct.price" class="text-decoration-line-through">{{ tempProduct.origin_price }}</p>
+              <p v-if="tempProduct.price" class="text-decoration-line-through">原價：NT$ {{ tempProduct.origin_price }}</p>
+              <p v-if="tempProduct.price" class="h4 fw-bolder">售價：NT$ {{ tempProduct.price }}</p>
+            </div>
+            <div class="input-group">
+              <input type="number" v-model.number="qty" min="1" class="form-control">
+              <button type="button" @click="$emit('add-cart', tempProduct.id, qty)" class="btn btn-hgray hvr-bounce-to-right">加入購物車</button>
             </div>
           </div>
         </div>
       </div>
-      <div class="modal-footer">
+      <!-- <div class="modal-footer">
         <button type="button" class="btn btn-hgray hvr-bounce-to-right">加入購物車</button>
-      </div>
+      </div> -->
     </div>
   </div>
 </div>
@@ -35,6 +41,8 @@ import * as bootstrap from 'bootstrap'
 
 export default {
   // 從外層傳入變數 product，(擔心單一數據流的問題)預計使用 tempProduct 來接收
+  // 如此一來，tempProduct 內容已和外層的 product 無關
+  // 若要避免 "單向數據流" 的問題，盡量不要用外層傳入的資料
   props: ['product'],
   data () {
     return {
--- src/views/back/BackShopping.vue
+++ src/views/back/BackShopping.vue
@@ -17,19 +17,18 @@
                 <h5 class="card-title mb-0 text-theme fw-bolder">{{ item.title }}</h5>
                 <span class="badge bg-secondary align-self-center">{{ item.category }}</span>
               </div>
-              <p class="card-text">{{ item.content }}</p>
               <div class="d-flex justify-content-between align-items-baseline">
                 <div class="h5" v-if="!item.price">{{ item.origin_price }} 元</div>
                 <del class="h6 text-streak" v-if="item.price">NT$ {{ item.origin_price }} 元</del>
                 <div class="h5 text-danger fw-bolder" v-if="item.price">NT$ {{ item.price }} 元</div>
               </div>
             </div>
-            <div class="card-footer bg-light d-flex justify-content-center px-0 py-2">
-              <button type="button" class="btn border-end btn-product-card hvr-bounce-to-right py-2 px-0 w-50">
+            <div class="card-footer position-relative bg-light d-flex align-items-center p-7">
+              <button type="button" @click="addCart(item.id)" class="btn position-absolute add-cart-btn zindex-1 border-end btn-product-card hvr-bounce-to-right py-2 px-0 w-50">
                 <i class="fas fa-shopping-cart me-1"></i>
                 加到購物車
               </button>
-              <button type="button" class="btn hvr-icon-wobble-vertical btn-product-card py-2 px-0 w-50">
+              <button type="button" class="btn position-absolute zindex-1 read-more-btn hvr-icon-wobble-vertical btn-product-card py-2 px-0 w-50">
                 <i class="fas fa-file-alt hvr-icon me-1"></i>
                 詳細資訊
               </button>
@@ -40,7 +39,7 @@
     </div>
   </div>
   <!-- Modal -->
-  <user-product-modal ref="userProductModal" :product="product"></user-product-modal>
+  <user-product-modal ref="userProductModal" :product="product" @add-cart="addCart"></user-product-modal>
 </section>
 </template>
 <script>
@@ -67,6 +66,18 @@ export default {
         } else {
           vm.$swal({ title: res.data.message, icon: 'error' })
         }
+      })
+    },
+    addCart (id, qty = 1) {
+      const vm = this
+      const cart = {
+        product_id: id,
+        qty
+      }
+      const api = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_CUSTOMPATH}/cart`
+      vm.$http.post(api, { data: cart }).then((res) => {
+        console.log('加入購物車', res.data, cart)
+        vm.$swal({ title: res.data.message, icon: 'success' })
       })
     },
     openModal (item) {
